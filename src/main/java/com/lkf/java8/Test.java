e88774a67a2dcf61660dbb54d016ef20956efa78
likaifeng <844701087@qq.com>
2017-11-27 19:42:43 +0800

    1

--- src/main/java/com/lkf/java8/Test.java
+++ src/main/java/com/lkf/java8/Test.java
@@ -16,10 +16,12 @@ public class Test {
         list.add("abc1defg");
         list1.addAll(list);
         
-        //list.removeIf(ele -> ele.length()>5);//从lis集合中删除长度大于5的元素
+        list.removeIf(ele -> ele.length()>5);//从lis集合中删除长度大于5的元素
         System.out.println(list.contains(""));//查看list集合中是否包含
-        /*list.stream().forEach(ele -> System.out.println(ele));
+        list.stream().forEach(ele -> System.out.println(ele));
         System.out.println("--------------------");
-        list.parallelStream().forEachOrdered(ele -> System.out.println(ele));*/
+        list.parallelStream().forEachOrdered(ele -> System.out.println(ele));
+
+
     }
 }
---
+++ src/main/java/com/lkf/java8/lambda.java
@@ -0,0 +1,46 @@
+package com.lkf.java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created by likaifeng on 2017/11/27.
+ */
+public class lambda {
+
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        apple.setColor("red");
+        apple.setWeight(2);
+        Apple apple1 = new Apple();
+        apple1.setColor("blue");
+        apple1.setWeight(5);
+        Apple apple2 = new Apple();
+        apple2.setColor("yellow");
+        apple2.setWeight(4);
+        List<Apple> list = new ArrayList<>();
+        list.add(apple);
+        list.add(apple1);
+        list.add(apple2);
+        List<String> colorList = list.stream()
+                .filter(apple3 -> {
+                    System.out.println(apple3.getWeight());
+                    return apple3.getWeight() > 3;
+                })//选出重量大于某个值的
+                .sorted(Comparator.comparing(Apple :: getWeight))//按照重量进项排序
+                .map(apple4 -> {
+                    System.out.println(apple4.getColor());
+                    return apple4.getColor();
+                })
+                .limit(1)//取出颜色
+                .collect(toList());
+        //colorList.forEach(color -> System.out.println(color));
+
+
+    }
+}
